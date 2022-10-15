c5eaa59a24f529c5fdd3a16e7a8592223c63f858
jmaczan <jedrzejpawel@maczan.pl>
2022-10-15 20:20:21 +0200

    Problem #3 and #4

--- problems.pl
+++ problems.pl
@@ -22,4 +22,23 @@ P03 (*) Find the K'th element of a list.
     ?- element_at(X,[a,b,c,d,e],3).
 */
 
-element_at(X, [H|T], Index) :- 
+element_at(X, [X|T], 1).
+element_at(X, [H|T], K) :- N is K - 1, element_at(X, T, N).
+
+/*
+P04 (*) Find the number of elements of a list.
+*/
+
+/*
+This solution can only verify length, i.e. list_length([1, 2], 2), but cannot calculate the length itself like list_length([1, 2], N) 
+*/
+list_length([], 0).
+list_length([H], 1).
+list_length([H|T], N) :- M is N - 1, list_length(T, M).
+
+/*
+This solution is able to do both; I think it's because an order of predicates inside the rule.
+*/
+another_list_length([], 0).
+another_list_length([H|T], N) :- another_list_length(T, M), N is M + 1.
+
